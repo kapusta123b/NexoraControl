686c14a1e6116007e93851a708263f57d7934841
Phonkmasti <fartuchoknik1@gmail.com>
2026-08-08 16:31:06 +0200

    feat(models): create and register new "Command" model

--- backend/apps/agents/admin.py
+++ backend/apps/agents/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from apps.agents.models.agent import Agent
+from apps.agents.models.command import Command
 
 
 
 
 admin.site.register(Agent)
+admin.site.register(Command)
---
+++ backend/apps/agents/models/command.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Command(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING"
+        RUNNING = "RUNNING"
+        SUCCESS = "SUCCESS"
+        FAILED = "FAILED"
+
+    agent = models.ForeignKey("agents.Agent", on_delete=models.CASCADE, related_name="commands")
+
+    command_type = models.CharField(max_length=50)
+
+    payload = models.JSONField(default=dict)
+
+    status = models.CharField(choices=Status.choices, default=Status.PENDING)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
