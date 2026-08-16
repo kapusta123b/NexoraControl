fa7c89e291ba521d2e202effa0c958e58e53dd5c
Phonkmasti <fartuchoknik1@gmail.com>
2026-08-16 16:32:06 +0200

    feat(registry): ddd validators to the command registry using a tuple

--- agent/commands/docker/registry.py
+++ agent/commands/docker/registry.py
@@ -1,10 +1,12 @@
 from .containers import *
 from .information import *
+from ..validators.docker import *
 
 COMMAND_DOCKER = {
     "docker_ps": docker_ps,
     "docker_ps_all": docker_ps_all,
-    "docker_logs": docker_logs,
+    "docker_logs": (docker_logs, docker_logs_validator),
     "docker_images": docker_images,
     "docker_stats": docker_stats,
+    "docker_inspect": (docker_inspect, docker_inspect_validator),
 }
--- agent/commands/system/registry.py
+++ agent/commands/system/registry.py
@@ -1,4 +1,5 @@
 from .system import *
+from ..validators.system import *
 
 COMMAND_SYSTEM = {
     "system_uptime": system_uptime,
@@ -6,6 +7,6 @@ COMMAND_SYSTEM = {
     "system_cpu_info": system_cpu_info,
     "system_ram_info": system_ram_info,
     "system_disk_info": system_disk_info,
-    "system_processes": system_processes,
+    "system_processes": (system_processes, system_processes_validator),
     "system_reboot": system_reboot,
 }
